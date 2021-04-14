0a10e5530d7a18e7b1718f145c667383ad0fb81e
nagyantal9312 <nagyantal9312@gmail.com>
2021-04-14 11:53:29 +0200

    processing crawled hungarian diseases data

---
+++ datacrawler/dataprocess.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+from unidecode import unidecode
+
+# TODO scrapy futtatas elott torolni kell az uj.json filet, mert maskepp a regi filehoz fuzi az ujat
+# TODO scrapy futtatas scriptbol
+# scrapy futtatas: koronavirus mappabol => scrapy crawl koronavirus -o uj.json
+
+
+def process_disease_hu():
+
+    unique_values = set()
+    diseases = pd.read_json('koronavirus/uj.json')
+    diseases['alapbetegsegek'].apply(lambda x: unique_values.update(x))
+
+    column = pd.Series(list(unique_values))
+
+    column = column.str.lower()
+    column = column.str.strip()
+    column = column.apply(lambda x: unidecode(x))
+    column.replace("", np.nan, inplace=True)
+    column.dropna(inplace=True)
+
+    column = column.drop_duplicates()
+    column = column.sort_values()
+    column.to_csv(path_or_buf='../data/local/diseases_hungarian3.csv', index=False)
+
+
+process_disease_hu()
